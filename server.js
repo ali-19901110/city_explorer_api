4d725fb0e8f0dadb3320fee05118b0ec8155180c
AliAlkasasbeh <alikasasbeh60@yahoo.com>
2021-03-29 02:28:29 +0300

    labb7

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const cors = require('cors'); 
 const app = express(); 
 app.use(cors());
-const port = 7000; 
-app.listen( port , ()=>{console.log('conected server .......')});
+const PORT = 7000; 
+app.listen( PORT , ()=>{console.log('conected server .......')});
 function CityExpoler(search_query, formatted_query, latitude, longitude) {
     this.search_query = search_query,
     this.formatted_query = formatted_query,
